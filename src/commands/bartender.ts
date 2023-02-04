f65d8c4c32e43268dec72f96586c2a0098fb4ff2
rKaic <czarevilsam@gmail.com>
2023-02-04 05:58:50 -0500

    Added types to bartender

--- src/commands/bartender.ts
+++ src/commands/bartender.ts
@@ -4,30 +4,109 @@ import { getJson, isNullOrEmpty, randomElement } from "../core";
 
 import { Command, CommandModule, ServiceCollection } from '../Types';
 
+export type CocktailInfo = {
+  idDrink: string
+  strDrink: string
+  strDrinkThumb: string
+}
+
+export type Cocktail = CocktailInfo & {
+  strCategory: string
+  strAlcoholic: string
+  strInstructions: string
+  strDrinkAlternate?: string
+  strTags?: string
+  strVideo?: string
+  strIBA?: string
+  strGlass?: string
+  strInstructionsES?: string
+  strInstructionsDE?: string
+  strInstructionsFR?: string
+  strInstructionsIT?: string
+  strIngredient1?: string
+  strIngredient2?: string
+  strIngredient3?: string
+  strIngredient4?: string
+  strIngredient5?: string
+  strIngredient6?: string
+  strIngredient7?: string
+  strIngredient8?: string
+  strIngredient9?: string
+  strIngredient10?: string
+  strIngredient11?: string
+  strIngredient12?: string
+  strIngredient13?: string
+  strIngredient14?: string
+  strIngredient15?: string
+  strMeasure1?: string
+  strMeasure2?: string
+  strMeasure3?: string
+  strMeasure4?: string
+  strMeasure5?: string
+  strMeasure6?: string
+  strMeasure7?: string
+  strMeasure8?: string
+  strMeasure9?: string 
+  strMeasure10?: string
+  strMeasure11?: string
+  strMeasure12?: string
+  strMeasure13?: string
+  strMeasure14?: string
+  strMeasure15?: string
+  strImageSource?: string
+  strImageAttribution?: string
+  strCreativeCommonsConfirmed?: string
+  dateModified: string
+};
+
+type SearchResponse = {
+  drinks?: Cocktail[]
+}
+
+type Ingredient = {
+  idIngredient: string
+  strIngredient: string
+  strDescription: string
+  strType: string
+  strAlcohol: string
+  strABV?: string
+}
+
+type FilterResponse = {
+  drinks?: CocktailInfo[]
+}
+
 // services is an object containing `logger`, `repo`, and `botManager` fields
 export default async (services: ServiceCollection): Promise<CommandModule[]> => {
   const apiKey = await cocktailDb.getApiKey();
   const baseUrl = `https://www.thecocktaildb.com/api/json/v1/${apiKey}`;
 
-  const getDrinksByName = async (recipeName: string): Promise<any[]> => {
-    const drinks: any[] = await getJson(`${baseUrl}/search.php?s=${encodeURIComponent(recipeName)}`);
-    return drinks || [];
+  const getDrinksByName = async (recipeName: string): Promise<Cocktail[]> => {
+    const response: SearchResponse = await getJson(`${baseUrl}/search.php?s=${encodeURIComponent(recipeName)}`);
+    return response?.drinks || [];
   };
 
-  const getDrinkById = async (id): Promise<any | undefined> => {
-    const drinks: any[] = await getJson(`${baseUrl}/lookup.php?i=${id}`);
-    return _.first(drinks);
+  const getDrinkById = async (id): Promise<Cocktail | undefined> => {
+    const response: SearchResponse = await getJson(`${baseUrl}/lookup.php?i=${id}`);
+    return _.first(response?.drinks || []);
   };
 
-  const getDrinksByIngredients = async (ingredients): Promise<any[]> => {
-    const drinks = await Promise.all(ingredients.map(i => getJson(`${baseUrl}/filter.php?i=${encodeURIComponent(i.trim())}`)));
-    let drinksInCommon = _.intersectionBy(...drinks, (drink) => {
+  const getDrinksByIngredients = async (ingredients): Promise<Cocktail[]> => {
+    const filterResponses: FilterResponse[] = await Promise.all(ingredients.map(i => getJson(`${baseUrl}/filter.php?i=${encodeURIComponent(i.trim())}`)));
+    let drinksInCommon = _.intersectionBy(...filterResponses.map(r =>r.drinks || []), (drink) => {
       return (drink).idDrink;
     });
-    return await Promise.all(drinksInCommon.map(d => getDrinkById(d.idDrink)));
+    const potentialDrinks = await Promise.all(drinksInCommon.map(d => getDrinkById(d.idDrink)));
+    const drinks: Cocktail[] = [];
+    for(let fullDrink of potentialDrinks) {
+      if(fullDrink != null) {
+        drinks.push(fullDrink)
+      }
+    }
+    return drinks;
   };
 
-  const formatDrink = (drink) => {
+  const formatDrink = (drink: Cocktail) => {
     let ingredients: string[] = [];
     for(let i = 1; i < 16; i++) {
       let ingredient = drink[`strIngredient${i}`];
@@ -56,7 +135,7 @@ export default async (services: ServiceCollection): Promise<CommandModule[]> => 
           return;
         }
         
-        let drinks: any[] = [];
+        let drinks: Cocktail[] = [];
         switch(command.params[0].toLowerCase()) {
           case "name":
             drinks = await getDrinksByName(command.params.slice(1).join(" "));
